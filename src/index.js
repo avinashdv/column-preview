cdca5f427970e7e3277dd16b718625be837c8fe5
Avinash DV <avinash.dv.264@gmail.com>
2022-10-29 16:58:16 +0530

    code re-write to reduce repetition and follow best practice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles/main.scss";
+import "./styles/_colors.scss";
+import "./styles/_fonts.scss";
+
 import iconSedans from "./assets/icon-sedans.svg";
 import iconSuvs from "./assets/icon-suvs.svg";
 import iconLuxury from "./assets/icon-luxury.svg";
-import favicon from "./assets/favicon.png";
 
 const sedan = document.getElementById("icon-sedan");
 const suv = document.getElementById("icon-suv");
